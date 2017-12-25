7d6062004b5921dd3258a63380f561eedfd68487
Romans Markuns <romanmarkunas@gmail.com>
2017-12-25 23:49:25 +0000

    add table booking service

--- booking.py
+++ booking.py
@@ -17,24 +17,24 @@ class Tables():
     decided that restraunt will be open on one single day between 12:00 and
     22:00, slot 12:00-13:00 being index 0 on the list and so on"""
 
-    __PAX_PER_TABLE = 2
-    __NO_TABLES = 2
-    __MAX_PAX = __PAX_PER_TABLE * __NO_TABLES
+    PAX_PER_TABLE = 2
+    NO_TABLES = 2
+    MAX_PAX = PAX_PER_TABLE * NO_TABLES
 
     def __init__(self):
         self.table1 = 10*[None]
         self.table2 = 10*[None]
 
     def check_available(self, slot, booking):
-        if (booking.pax > Tables.__MAX_PAX):
+        if (booking.pax > Tables.MAX_PAX):
             return False
-        elif (booking.pax <= Tables.__PAX_PER_TABLE):
+        elif (booking.pax <= Tables.PAX_PER_TABLE):
             return self.__free(self.table1, slot) or self.__free(self.table2, slot)
         else:
             return self.__free(self.table1, slot) and self.__free(self.table2, slot)
 
     def book(self, slot, booking):
-        if (booking.pax > Tables.__PAX_PER_TABLE):
+        if (booking.pax > Tables.PAX_PER_TABLE):
             self.table1[slot] = booking
             self.table2[slot] = booking
         elif self.__free(self.table1, slot):
---
+++ booking_service.py
@@ -0,0 +1,59 @@
+from booking import Tables, Booking
+
+class BookingService():
+    """Booking service object to be used from endpoint layer"""
+
+    def __init__(self):
+        self.tables = Tables()
+
+    def book(self, hour, pax, customer_number, alternatives):
+        """Returns true if booking was successful, otherwise returns false and
+        populates alternatives list with alternative free bookings
+
+        hour - booking start hour in 24h system
+        pax - number of people in booking
+        customer_number - customer telephone number
+        alternatives - list variable to store alternative booking options"""
+
+        slot = hour - 12;
+        initial_booking = Booking(customer_number, pax)
+
+        if (self.tables.check_available(slot, initial_booking)):
+            self.tables.book(slot, initial_booking)
+            return True
+        else:
+            alt = self.__generate_alternatives(slot, initial_booking)
+            self.__copy_list(alt, alternatives)
+            return False
+
+    def get_tables(self):
+        return self.tables.get_tables()
+
+    def __generate_alternatives(self, slot, booking):
+        current_pax = booking.pax
+        alternatives = []
+
+        while ((not alternatives) or (current_pax <= 0)):
+            for alt_slot in range(max(0, slot - 1), min(slot + 2, 12)):
+                alt_booking = Booking(booking.customer_number, current_pax)
+                alternatives.append((alt_slot, alt_booking))
+            alternatives = self.__filter_unavailable(alternatives)
+            current_pax -= 1
+
+        self.__sort_slot_closer_to_init(alternatives, slot)
+        return alternatives
+
+    def __filter_unavailable(self, bookings):
+        available = []
+        for booking in bookings:
+            if (self.tables.check_available(booking[0], booking[1])):
+                available.append(booking)
+        return available
+
+    def __sort_slot_closer_to_init(self, bookings, initial_slot):
+        bookings.sort(key = lambda t: abs(t[0] - initial_slot))
+
+    def __copy_list(self, src, dst):
+        del dst[:]
+        for item in src:
+            dst.append(item)
