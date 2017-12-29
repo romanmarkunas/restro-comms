bf10491b20595ee2e72779637cc408b6456169a7
Romans Markuns <romanmarkunas@gmail.com>
2017-12-29 14:54:39 +0000

    typo

--- ncco_server.py
+++ ncco_server.py
@@ -193,6 +193,6 @@ class NCCOServer():
         with open("static/dashboard.html") as page:
             return page.read()
 
-if __name__ == "__main__"
+if __name__ == "__main__":
     router = hug.route.API(__name__)
     router.object('/')(NCCOServer)
