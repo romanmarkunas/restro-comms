725e718c2ce358c7534f7f1d524561b32db8f3e9
Romans Markuns <romanmarkunas@gmail.com>
2017-12-30 20:40:54 +0000

    typo

--- ncco_server.py
+++ ncco_server.py
@@ -156,7 +156,7 @@ class NCCOServer():
     @hug.object.get('/remind/start')
     def remind_start_ncco(self, body = None):
         booking_id = self.outbound_uuid_to_booking(body["uuid"])
-        time = self.booking_service.find(booking_id[0]
+        time = self.booking_service.find(booking_id[0])
         return [{
             "action": "talk",
             "voiceName": "Russell",
